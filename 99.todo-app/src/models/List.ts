deb7c9b01b6e140b91bd4ee40c74ba2960c78547
leejuno <zhqkfxm0127@naver.com>
2024-01-22 16:34:43 +0900

    feat(src/models): todoapp 데이터 모델 생성

---
+++ 99.todo-app/src/models/List.ts
@@ -0,0 +1,67 @@
+import ListItem, { IListItem } from "./ListItem";
+
+
+interface IList{
+    list: IListItem[];
+    load(): void;
+    save(): void;
+    clearList(): void;
+    addItem(itemObj: IListItem): void;
+    removeItem(id: string): void;
+}
+
+export default class List implements IList{
+    
+    static instace: List = new List();
+
+    private constructor(
+        private _list: ListItem[] = []
+    ){}
+
+    get list() : ListItem[]{
+        return this._list;
+    }
+
+    load(): void{
+        const storedList: string | null = localStorage.getItem('myList');
+
+        if(typeof storedList !== 'string') return;
+
+        const parsedList:{
+            _id: string,
+            _item: string,
+            _checked: boolean
+        }[] = JSON.parse(storedList);
+
+        // ListItem 인스턴스 객체 생성 => List 인스턴스 객체에 넣어주기
+        parsedList.forEach(itemObj =>{
+            const newListItem = new ListItem(
+                itemObj._id,
+                itemObj._item,
+                itemObj._checked
+            )
+            List.instace.addItem(newListItem);
+        })
+
+
+    }
+    // localstorage에 저장해주는 메소드
+    save():void{
+        localStorage.setItem('myList', JSON.stringify(this._list));
+    }
+
+    clearList(): void {
+        this._list = [];
+        this.save();
+    }
+
+    addItem(itemObj: ListItem): void{
+        this._list.push(itemObj);
+        this.save();
+    }
+
+    removeItem(id: string): void{
+        this._list = this._list.filter(item => item.id !== id);
+        this.save();
+    }
+}
